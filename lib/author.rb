ff5f0bf54807c392d7e280905c9530ff0c663100
RikBala <rik280491@googlemail.com>
2020-01-10 13:56:43 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author 
+
+    attr_accessor :name 
+
+    def initialize
+        @name = name 
+    end 
+
+
+
+
+
+
+
+
+
+
+end 
---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+ 
+    attr_accessor :title, :author 
+
+    def initialize
+        @title = title 
+        @author = author
+    end 
+
+
+
+
+
+
+
+
+
+
+end 
